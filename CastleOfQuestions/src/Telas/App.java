c8bf51a260c299233465723242f6c8380da596c0
Mateus_Suzena <projeto.integrador.flavio@gmail.com>
2018-09-15 16:23:43 -0300

    Commit inicial do projeto. Primeira versão Beta contendo apenas a logica do akinator.

--- CastleOfQuestions/src/Telas/App.java
+++ CastleOfQuestions/src/Telas/App.java
@@ -8,5 +8,6 @@ public class App {
     public static void main(String[] args) {
         GameRun.start();
         System.out.println("Pora bicho!");
+        System.out.println("Pora bicho! faz esse commit desgracado");
     }   
 }
