f7d02a4e049dfdc4b4a3252b2c43dcb9be584e7b
Mateus_Suzena <vmsuzena5@gmail.com>
2018-09-19 23:30:03 -0300

    Implementacao da funcao "Reconhecimento Facial" Foram adicionados os seguintes itens: package "fotos" - contendo fotos minha (Mateus), para meu reconhecimento package "reconhecimento" - contendo as classes: 	- Classe CapturaImagens -> Para capturar as imagens de quem sera reconhecido (Sendo necessario rodar duas vezes o arquivo, na primeira colocar o id como "1" e na segunda como "2" 	- Classe Treinamento -> Esta deve ser executada apos ter realizado a captura das imagens com a classe CapturaImagens, a classe Treinamento ira treinar os classificadores. 	- Classe Reconhecedor -> executa a logica de reconhecimento e returna um boolean
    package "recursos.classificadores" -> contendo os classificadores para os tres algoritmos de reconhecimento facial
    	-

--- CastleOfQuestions/src/Telas/App.java
+++ CastleOfQuestions/src/Telas/App.java
@@ -1,11 +1,23 @@
 package Telas;
 
 import Classes.*;
+import java.util.Scanner;
+import org.bytedeco.javacv.FrameGrabber;
+import reconhecimento.Reconhecedor;
 
 public class App {
     
-    public static void main(String[] args) {
-        GameRun.start();
+    public static void main(String[] args) throws FrameGrabber.Exception, InterruptedException {
+        boolean reconhecido;
+        Scanner scanner = new Scanner(System.in);
+        Reconhecedor recog = new Reconhecedor();
+        reconhecido = recog.reconhece(); 
+        if (reconhecido){
+            GameRun.start();
+            reconhecido = recog.reconhece();
+        }else{
+            System.out.println("Voce nao foi reconhecido");
+        } 
     }
     
 }
